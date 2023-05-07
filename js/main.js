485964f3efe1b755a229a86417e38daebb490b22
muriloapolo <massps2@gmail.com>
2023-05-07 13:40:43 -0300

    Adicionado aviso de cokies e app transferido para CLASSE

--- js/main.js
+++ js/main.js
@@ -1,55 +1,91 @@
 // const url = "https://digimon-api.com/api/v1/digimon/";
-const home = document.querySelector(".btn-home");
-const next = document.querySelector(".btn-next");
-const prev = document.querySelector(".btn-prev");
-const digiId = document.querySelector(".digiId");
-const digiName = document.querySelector(".digiName");
-const tela = document.querySelector(".digi-echo");
+// const home = document.querySelector(".btn-home");
+// const next = document.querySelector(".btn-next");
+// const prev = document.querySelector(".btn-prev");
+// const digiId = document.querySelector(".digiId");
+// const digiName = document.querySelector(".digiName");
+// const tela = document.querySelector(".digi-echo");
 // let ind = 0;
-let id = 1;
-
+// let id = 1;
 const dataBase = async (digi) => {
   //digiData é o paramentro da API [id Digimon unique id ] [name Digimon's name]
-
   const data = await fetch(`https://digimon-api.com/api/v1/digimon/${digi}`);
   if (data.status == 200) {
     const dataString = await data.json();
     return dataString;
   }
-
-  // console.log(dataString);
-  // digiTela(dataString);
 };
-const digiConsole = async (digi) => {
-  const dataD = await dataBase(digi);
-  if (dataD) {
-    digiId.innerText = dataD.id;
-    digiName.innerText = dataD.name;
-    digiTela(dataD);
-  }
-};
-
-const digiTela = (dataD) => {
-  // tela.src = `${dataD.images[0].href}`;
-  tela.src = dataD.images[0].href;
-};
-
-next.addEventListener("click", () => {
-  if (id > 1422) {
-    id = 1;
-    digiConsole(id);
-  }
-  id++
-  digiConsole(id);
-
-});
-
-prev.addEventListener("click", () => {
-  if (id > 1) {
-    id--
-    digiConsole(id);
+class Digivice {
+  constructor(dataBase) {
+    this.ind = 1;
+    this.digiId = dataBase.id;
+    this.digiName = dataBase.name;
   }
 
-});
-//RENDER POKE
-digiConsole(id);
+  //Gera Botões
+  buttons() {
+    const home = document.querySelector(".btn-home");
+    const next = document.querySelector(".btn-next");
+    const prev = document.querySelector(".btn-prev");
+    this.listeners(next, prev, home);
+  }
+  //Textos e Imagem
+  display() {
+    const digiId = document.querySelector(".digiId");
+    const digiName = document.querySelector(".digiName");
+    const tela = document.querySelector(".digi-echo");
+    this.digiConsole(this.ind,digiId, digiName,tela);
+    // this.digitela(tela)
+  }
+  listeners(next, prev, home) {
+    //NEXT
+    next.addEventListener("click", () => {    
+      if (this.ind > 1422) {
+        this.ind = 1;
+        // this.digiConsole(this.ind);
+        this.display()
+       
+      }
+      this.ind++;
+      // this.digiConsole(this.ind);
+      this.display()
+    });
+    // PREV
+    prev.addEventListener("click", () => {
+    
+      if (this.ind > 1) {
+        this.ind--;
+        // this.digiConsole(this.ind);
+        this.display()
+      }
+    });
+  }
+  async digiConsole(digi,digiId,digiName,tela) {
+    
+    let dataD = await dataBase(digi);
+    if (dataD) {
+      digiId.innerText = dataD.id;
+      digiName.innerText = dataD.name;
+      tela.src = dataD.images[0].href;
+    
+    }
+  }
+}
+dataBase(1)
+const newDigivice = new Digivice(dataBase, 1);
+newDigivice.buttons();
+newDigivice.display();
+
+
+if (!localStorage.getItem("cookiesAccepted")) { 
+  var cookieMessage = document.getElementById('cookie-notification');  
+  var closeCookie = document.getElementById('cookie-notification-close');
+  
+  cookieMessage.style.display = 'block';  
+  closeCookie.addEventListener("click", function(e) {  
+    e.preventDefault();
+    localStorage.setItem("cookiesAccepted", true);
+    
+    cookieMessage.style.display = 'none';
+  });
+}
